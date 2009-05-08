5a397f3743dfdb9b5cc1a680d7bca0eaf2bf98b7
Nick Dunn <nickdunn@nick-dunn-2.local>
2009-05-08 12:58:08 +0100

    JS ponderings

--- assets/apply.js
+++ assets/apply.js
@@ -3,71 +3,100 @@ $(document).ready(function() {
 	var total = 0;
 	var complete = 0;
 	
-	$('label.file > span').each(function(i) {
-		var file = $(this);
+	var uploadifies = [];
+	var form = $('form');
+	
+	var file_fields = $('label.file');
+	
+	file_fields.each(function(i) {
+		
+		var label = $(this);
+		var file = $(this).find(' > span');
 		var input = $('input', file);
 		
-		var types = '';
-		var destination = '';
-		for(var field in upload_fields) {
-			if ('fields[' + upload_fields[field].handle + ']' == input.attr('name')) {
-				types = upload_fields[field].types;
-				destination = upload_fields[field].destination;
-			};
-		}
-		
-		file.append('<div class="uploadify" id="uploadify-' + i + '">TEST</div>')
-		.find('.uploadify').fileUpload({ 
-			'uploader': '/extensions/uploadify/assets/uploader.swf',
-			'script': '/extensions/uploadify/assets/upload.php',
-			'buttonImg': '/extensions/uploadify/assets/choose.png',
-			'width': 68,
-			'height': 15,
-			'folder': destination,
-			'wmode': 'transparent',
-			scriptAccess: 'always',
-			fileExt: types,
-			buttonText: 'Browse files',
-			
-			onError: function(event, queueId, fileObj, errorObj) {
-				console.dir(errorObj);
-			},
-			
-			onComplete: function(event, queueId, fileObj, response, data) {
-				file.append('<input type="hidden" name="' + input.attr('name') + '" value="' + fileObj.filePath.replace(/\/workspace/,'') + '" />');
-				registerComplete();
+			var upload_file_types = '';
+			var upload_destination = '';
+			for(var field in upload_fields) {
+				if ('fields[' + upload_fields[field].handle + ']' == input.attr('name')) {
+					upload_file_types = upload_fields[field].types;
+					upload_destination = upload_fields[field].destination;
+				};
 			}
 
-		});
-		
-		// if existing file
-		if ($('a', this).length) {
-			
-			$('.fileUploaderBtn', file).hide();
-			
-			// rather than unbinding the "Remove file" text, clone it and re-append with a new click event
-			var remove_file = $('em', this);
-			var remove_clone = remove_file.clone();
-			remove_file.remove();
-			remove_clone.click(function() {
-				$('.fileUploaderBtn', file).show();
-				$('a', file).hide();
-				$(this).remove();
-				total++;
-			})
-			$('a', this).after(remove_clone);
+			file.append('<div class="uploadify" id="uploadify-' + i + '">TEST</div>')
+			.find('.uploadify').fileUpload({ 
+				
+				'uploader': '/extensions/uploadify/assets/uploader.swf',
+				'script': '/extensions/uploadify/assets/upload.php',
+				
+				'folder': upload_destination,
+				fileExt: upload_file_types,
+				sizeLimit: $('input[name="MAX_FILE_SIZE"]', form).val(),
+				
+				'buttonImg': '/extensions/uploadify/assets/choose.png',
+				'width': 68,
+				'height': 15,
+				'wmode': 'transparent',
+				
+				scriptAccess: 'always',
 
-		} else {
-			
-			input.remove();
-			total++;
-			
-		}
+				onSelectOnce: function(event, data) {
+					
+					if (data.fileCount == 1) {
+						if ($.inArray("uploadify-" + i, uploadifies)) {
+							uploadifies.push("uploadify-" + i);
+						}
+					}
+
+					if (label.parent().hasClass('invalid')) {
+						
+						//label.find('+ p').remove();
+					}
+					
+				},
+
+				onError: function(event, queueId, fileObj, errorObj) {
+					switch (errorObj.type) {
+						case 'File Size':
+							wrapFormElementWithError(label, '<div class="invalid"></div>').after('<p>File chosen exceeds the maximum allowed upload size of ' + errorObj.sizeLimit + ' bytes specified by Symphony.</p>');
+						break;
+					}
+				},
+
+				onComplete: function(event, queueId, fileObj, response, data) {
+					file.append('<input type="hidden" name="' + input.attr('name') + '" value="' + fileObj.filePath.replace(/\/workspace/,'') + '" />');
+					registerComplete();
+				}
+
+			});
+
+			// if existing file
+			if ($('a', this).length) {
+
+				$('.fileUploaderBtn', file).hide();
+
+				// rather than unbinding the "Remove file" text, clone it and re-append with a new click event
+				var remove_file = $('em', this);
+				var remove_clone = remove_file.clone();
+				remove_file.remove();
+				remove_clone.click(function() {
+					$('.fileUploaderBtn', file).show();
+					$('a', file).hide();
+					$(this).remove();
+					total++;
+				})
+				$('a', this).after(remove_clone);
+
+			} else {
+
+				input.remove();
+				total++;
+
+			}
 		
 	});
 	
-	$('form').submit(function(e){
-
+	form.submit(function(e){
 		// TODO: handle required fields
 		$('.uploadify').each(function() {
 			var self = $(this);
@@ -80,11 +109,18 @@ $(document).ready(function() {
 	
 	function registerComplete() {
 		complete++;
-		console.log([complete, total]);
 		if (complete == total) {
-			$('form').unbind('submit');
-			$('form input[type="submit"]').click();
+			form.unbind('submit');
+			form.find('form input[type="submit"]').click();
 		}
+	}
+	
+	function wrapFormElementWithError(element, message) {
+		if (element.parent().hasClass('invalid')) {
+			element.parent().find('p').html(message);
+		} else {
+			element.wrap('<div class="invalid"></div>').after('<p>' + message + '</p>');
+		}		
 	}
 	 
 });
